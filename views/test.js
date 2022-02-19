991e50d91903f4df22c7ae6f9b0b5c2717831434
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-02-19 19:55:16 +0100

    ajout_event fini et tester : OK!!!

--- views/test.js
+++ views/test.js
@@ -7,15 +7,16 @@ class SetNewEvent {
         this.description = description;
         this.beginDate = beginDate;
         this.endDate = endDate;
-            }
-        }
+    }
+}
 function getActualDate() {
-        return new Date();
-    }
+    return new Date();
+}
+
 function checkBeginEnd(_beginDate, _endDate) {
-        /* vérifier que la date de début est avant la date de fin de l'évènement*/
-        return _beginDate < _endDate;
-    }
+    /* vérifier que la date de début est avant la date de fin de l'évènement*/
+    return _beginDate < _endDate;
+}
 
 const form = document.getElementById('test-form');
 const message = document.querySelector('.message');
@@ -25,8 +26,8 @@ function interceptForm(event) {
 
     const title = form[0].value;
     const location = form[1].value;
-    const beginDate = form[2].value;
-    const endDate = form[3].value;
+    const beginDate = new Date(form[2].value);
+    const endDate = new Date(form[3].value);
     const category = form[4].value;
     const description = form[5].value;
     var txtToComplete = 'Veuillez completer : ';
@@ -38,15 +39,28 @@ function interceptForm(event) {
     console.log(category);
     console.log(description);
 
-    if (title.lenght == 0 ){txtToComplete += ' le titre de l\'évenement', complete = false;};
-    if (location.lenght == ){txtToComplete += ' la localisation', complete = false;};
-    if (!checkBeginEnd(beginDate , endDate) || !checkBeginEnd(getActualDate(), beginDate)) {
-        txtToComplete += ' les date de manière correcte', complete = false;};
-    if (category.length == 0){txtToComplete += ' la catégorie', complete = false;};
-    if (complete){
-        return new SetNewEvent(title, beginDate, endDate,category,location,description)
+    if (title.lenght === 0) {
+        txtToComplete += ' le titre de l\'évenement';
+        complete = false;
     }
-    else{ message.innerHTML = txtToComplete; return null;};
+    if (location.lenght === 0) {
+        txtToComplete += ' la localisation';
+        complete = false;
+    }
+    if (!checkBeginEnd(beginDate, endDate) || !checkBeginEnd(getActualDate(), beginDate)) {
+        txtToComplete += ' les date de manière correcte'; complete = false;
+    }
+    if (category.length === 0) {
+        txtToComplete += ' la catégorie';
+        complete = false;
+    }
+    if (complete) {
+        return new SetNewEvent(title, beginDate, endDate, category, location, description)
+    }
+    else {
+        message.innerHTML = txtToComplete;
+        return null;
+    }
 }
 
 form.addEventListener('submit', interceptForm);
