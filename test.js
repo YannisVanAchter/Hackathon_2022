a82efbbd994d67f7782f038809f1659cbbb1034d
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-02-19 10:10:32 +0100

    Ajout du .gitignore 1-package-lock.json 2-package.json

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-fonction set_profile (name , Sur_name , )
+// import { now } from "jQuery";
+
+function getDate (dateActual) {
+    
+}
+
+const dateActual = new Date();
+console.log("Nous sommes le : " + dateActual)
+
+function setNewEvent (name , date , beginHour , endHour , categorie ) {
+    return 0
+};
