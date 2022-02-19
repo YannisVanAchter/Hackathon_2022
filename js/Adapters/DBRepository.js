45c376e31c74021f4504c619af21a0437f958ef0
lenaicl <l.landenne@yahoo.com>
2022-02-19 10:31:48 +0100

    feat : Redirect from index to login && basic adapter

---
+++ js/Adapters/DBRepository.js
@@ -0,0 +1,32 @@
+class DBRepository{
+
+  constructor() {
+    //TODO Seed database with default values
+    console.log("Seeding indexedDB");
+  }
+
+
+  // =========== USERS =============================
+  static addUser(userObject) {
+
+  }
+
+  
+  static doesLoginExist(email, password, callback) {
+
+  }
+
+
+  //  =========== EVENTS =============================
+  static addEvent(eventObject) {
+
+  }
+
+
+  //  =========== EVENT PARTICIPATION =============================
+  static addEventParticipation(addEventParticipationObject) {
+
+  }
+
+
+}
