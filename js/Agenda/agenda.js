05cef8ad9d7031822009fa65fb7f8346d61f49a3
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-02-20 03:52:57 +0100

    Finalisation de l'agenda test : OK

--- js/Agenda/agenda.js
+++ js/Agenda/agenda.js
@@ -1,43 +1,89 @@
 
-const week = [
+const weeks = [
+    "Dimanche",
     "Lundi",
     "Mardi",
     "Mercredi",
     "Jeudi",
     "Vendredi",
-    "Samedi",
-    "Dimanche"
+    "Samedi"
 ];
 
-const today = new Date()
+const today = new Date();
+
+// function actived to write the days of the week
+function writeWeek(date){
+    // initialisation des ressources mémoire
+    let semaine_tag = document.querySelector('.semaine')
+    let actualDate = new Date(date)
+    let monday = getMonday(actualDate)
+    let sunday = getSunday(monday)
+
+    // met à jour l'entête de l'agenda (bleu)
+    mondayTxt = ((monday.getDate() +1) + '/' + (monday.getMonth() + 1) );
+    sundayTxt = ((sunday.getDate() + 1) + '/' + (sunday.getMonth() +1 ));
+    console.log(mondayTxt + ' - ' + sundayTxt);
+    semaine_tag.innerHTML = (mondayTxt + ' - ' + sundayTxt);
+
+
+    // met à jour le corp de l'agenda (blanc)
+    for (let i = 1; i<8 ; i++) {
+        // ajout des jours
+        const day_tag = document.querySelector('.d_'+i);
+        if (i-1 == actualDate.getDay()) {
+            day_tag.classList.add('current-date');
+        }
+
+        // ajout des evènements
+        const event_tag = document.querySelector(".a_"+i)
+        // faire vérifier la db avec Lenaïc
+        // event_tag.innerHTML = EventParticipation()
+    }
+}
 
 function getMonday(d) {
     d = new Date(d);
-    var day = d.getDay(),
+    let day = d.getDay(),
         diff = d.getDate() - day;
     return new Date(d.setDate(diff));
 }
   
 function getSunday(d) {
     d = new Date(d);
-    var day = d.getDay(),
+    let day = d.getDay(),
         diff = d.getDate() - day + 6;
     return new Date(d.setDate(diff));
 }
 
-var headCalendar = "Semaine du " + getMonday(current) + " au " + getSunday(current);
+// let headCalendar = "Semaine du " + getMonday(current) + " au " + getSunday(current);
 
-var weekCalendar = {
+// let weekCalendar = {
     
+// }
+
+function prevW(event) {
+    // preventDefault() = 
+    event.preventDefault();
+    let date = new Date();
+    date.setDate()-= 7;
+    writeWeek(date);
 }
+
+function nextW(event) {
+    event.preventDefault();
+    let date = new Date();
+    date = new Date(date - 21*24*60*60);
+    writeWeek(date);
+}
+
+function actualW(event){
+    event.preventDefault();
+    let date = new Date();
+    writeWeek(date);
+}
+
+// set connection to div with id 'contrat_mariage' for all tag inside
 document.getElementById("prevWeek").addEventListener("click", prevW);
 document.getElementById("nextWeek").addEventListener("click", nextW);
-
-function prevW() {
-    date -= 7;
-}
-
-function nextW() {
-    date += 7;
-}
-
+// remplace BALISE par la balise HTML depuis l'affichage de la map
+// document.getElementById(BALISE).addEventListener("click", actualW);
